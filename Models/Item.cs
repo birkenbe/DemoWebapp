58db557780b160399c745744e34d6a7f804f0750
Martin <Martin@DESKTOP-P37O4UN>
2022-07-08 18:11:46 +0200

    Add project files.

---
+++ Models/Item.cs
@@ -0,0 +1,16 @@
+﻿namespace DemoWebapp.Models
+{
+    public class Item
+    {
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Item()
+        {
+
+        }
+
+    }
+}
